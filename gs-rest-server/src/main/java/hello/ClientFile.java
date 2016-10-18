ae5d4924ac0c9570233f925382d9c8f309057d38
leald <leald@carleton.edu>
2016-10-18 14:30:13 -0500

    Makes the two 'ClientFile' classes exactly the same

--- gs-rest-server/src/main/java/hello/ClientFile.java
+++ gs-rest-server/src/main/java/hello/ClientFile.java
@@ -1,18 +1,9 @@
 package hello;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class ClientFile {
 	private static String fileContent;
 	private static String errorMessage;
 
-	// @JsonCreator
-	// public ClientFile(@JsonProperty("fileContent") String file, @JsonProperty("errorMessage") String errorMessage) {
-	// 	this.fileContent = file;
-	// 	this.errorMessage = errorMessage;
-	// }
 	public ClientFile() {}
 
 	public ClientFile(String file, String errorMessage) {
