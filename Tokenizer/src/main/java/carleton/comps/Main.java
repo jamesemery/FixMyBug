4c8951e20c05966d60444f4b22a697215a673815
Alex Griese <griesea@carleton.edu>
2016-10-19 17:29:26 -0500

    Got basic functionality working for tokenizer, need to comment/clean and add betweenLines funcitonality.
    Basically complete functionality, but code needs to be cleaned and commented.

--- Tokenizer/src/main/java/carleton/comps/Main.java
+++ Tokenizer/src/main/java/carleton/comps/Main.java
@@ -5,6 +5,7 @@ import carleton.comps.javaparser.JavaParser;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import java.util.List;
 
 import java.io.IOException;
 
@@ -12,15 +13,21 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            ANTLRFileStream input = new ANTLRFileStream("/Users/jamie/Documents/College/Senior/Comps/FixMyBug/Tokenizer/src/main/java/carleton/comps/Main.java");
+            ANTLRFileStream input = new ANTLRFileStream("/Users/alexgriese/College/CS/CompsSeminar/FixMyBug/Tokenizer/src/main/java/carleton/comps/javaparser/examples/HelloWorld.java");
             JavaLexer lexer = new JavaLexer(input);
             CommonTokenStream stream = new CommonTokenStream(lexer);
-            System.out.println(stream.getNumberOfOnChannelTokens());
+            //System.out.println(stream.getNumberOfOnChannelTokens());
             for (Token t : stream.getTokens()) {
-                System.out.println(t.getType() +"(" + JavaParser.VOCABULARY
-                        .getSymbolicName(t.getType()) + ") " + t.getText());
+                //System.out.println(t.getType() +"(" + JavaParser.VOCABULARY
+                 //.getSymbolicName(t.getType()) + ") " + t.getText());
             }
-            System.out.println(stream.getTokens());
+            //System.out.println(stream.getTokens());
+
+
+            TokenizerBuilder t = new TokenizerBuilder("/Users/alexgriese/College/CS/CompsSeminar/FixMyBug/Tokenizer/src/main/java/carleton/comps/javaparser/examples/HelloWorld.java","file");
+            //System.out.println(t.getString());
+            List<Token> l = t.betweenLines(1,2);
+            System.out.println(t.tokensToString(l));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Tokenizer/src/main/java/carleton/comps/TokenizerBuilder.java
+++ Tokenizer/src/main/java/carleton/comps/TokenizerBuilder.java
@@ -3,11 +3,15 @@ package carleton.comps;
 import carleton.comps.javaparser.JavaLexer;
 import carleton.comps.javaparser.JavaParser;
 import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 /**
  * Chose a builder API
@@ -19,24 +23,47 @@ public class TokenizerBuilder {
     String fileSource;
     JavaLexer lexer;
     List<Token> data;
+    List<Token> tokenizedLines;
+    List<JavaLexer> lexedLines;
 
     // represents whether or not the lexer has already filled its input
     boolean lexed;
 
     //TODO open up the API to accept any sort of character stream as input
-    public TokenizerBuilder(String file) throws IOException {
-        lexer = new JavaLexer(new ANTLRFileStream(file));
-        // array of lines, send javalexer each line, store lexed line in array
+    public TokenizerBuilder(String code, String type) throws IOException {
+        if (type.equals("file")){
+            lexer = new JavaLexer(new ANTLRFileStream(code));
+            // array of lines, send javalexer each line, store lexed line in array
+
+            // Goes through each line of code and converts it into lexed code.
+            Scanner scanner = new Scanner(new File(code));
+            lexedLines = new ArrayList<JavaLexer>();
+            while (scanner.hasNextLine()) {
+                lexedLines.add(new JavaLexer(new ANTLRInputStream(scanner.nextLine())));
+            }
+        }
     }
 
     /**
      * Narrows down the tokenizer to the right set of lines
      * @return
      */
-    public TokenizerBuilder betweenLines(int start, int stop) {
+    public List<Token> betweenLines(int start, int stop) {
         // go to indexed lines
         if (lexed); //TODO something
-        return this;
+        CommonTokenStream s;
+        List<Token> list;
+        tokenizedLines = new ArrayList<Token>();
+        for (int i = start; i<=stop; i++) {
+            s = new CommonTokenStream(lexedLines.get(i));
+            s.getNumberOfOnChannelTokens();
+            list = s.getTokens();
+
+            for (Token t : list) {
+                tokenizedLines.add(t);
+            }
+        }
+        return tokenizedLines;
 
     }
 
@@ -47,8 +74,20 @@ public class TokenizerBuilder {
         StringBuilder builder = new StringBuilder();
         for (Token t : data) {
             builder.append(t.getType() +"(" + JavaParser.VOCABULARY
-                    .getLiteralName(t.getType()) + ") ");
+                    .getSymbolicName(t.getType()) + ") " + "(" + t.getText() + ") \n");
         }
+
+        return builder.toString();
+    }
+
+    public String tokensToString(List<Token> tokens) {
+
+        StringBuilder builder = new StringBuilder();
+        for (Token t : tokens) {
+            builder.append(t.getType() +"(" + JavaParser.VOCABULARY
+                    .getSymbolicName(t.getType()) + ") " + "(" + t.getText() + ") \n");
+        }
+
         return builder.toString();
     }
 
@@ -59,9 +98,14 @@ public class TokenizerBuilder {
         return data;
     }
 
+    /*
+     * Generates the tokens based on the code in the lexer.
+     */
+
     public void generateTokens() {
         CommonTokenStream stream = new CommonTokenStream(lexer);
         data = stream.getTokens();
+        stream.getNumberOfOnChannelTokens();
         lexed = true;
     }
 }
