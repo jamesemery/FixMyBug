40ca7e7ad2db269e4749671ceb98fb838f1a85ae
Alex Griese <griesea@carleton.edu>
2016-10-23 16:30:14 -0500

    Commented, cleaned and added in string functionality

--- Tokenizer/src/main/java/carleton/comps/Main.java
+++ Tokenizer/src/main/java/carleton/comps/Main.java
@@ -1,33 +1,68 @@
+
+
 package carleton.comps;
 
-import carleton.comps.javaparser.JavaLexer;
-import carleton.comps.javaparser.JavaParser;
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import java.util.List;
 
 import java.io.IOException;
 
+/*
+ * Checks if the TokenizerBuilder class works.
+ * @author Alex Griese
+ * @author Jamie Emery
+ */
+
 public class Main {
 
     public static void main(String[] args) {
         try {
-            ANTLRFileStream input = new ANTLRFileStream("/Users/alexgriese/College/CS/CompsSeminar/FixMyBug/Tokenizer/src/main/java/carleton/comps/javaparser/examples/HelloWorld.java");
-            JavaLexer lexer = new JavaLexer(input);
-            CommonTokenStream stream = new CommonTokenStream(lexer);
-            //System.out.println(stream.getNumberOfOnChannelTokens());
-            for (Token t : stream.getTokens()) {
-                //System.out.println(t.getType() +"(" + JavaParser.VOCABULARY
-                 //.getSymbolicName(t.getType()) + ") " + t.getText());
-            }
-            //System.out.println(stream.getTokens());
 
+            // Testing the file type of the constructor.
 
-            TokenizerBuilder t = new TokenizerBuilder("/Users/alexgriese/College/CS/CompsSeminar/FixMyBug/Tokenizer/src/main/java/carleton/comps/javaparser/examples/HelloWorld.java","file");
+            // Creates a new instance of TokenizerBuilder.
+            TokenizerBuilder t = new TokenizerBuilder("/Users/alexgriese/College/CS/CompsSeminar/FixMyBug/Tokenizer/src/main/java/carleton/comps/javaparser/examples/HelloWorld.jav","file");
+
+            // Prints the tokenized version of HelloWorld.java.
             //System.out.println(t.getString());
-            List<Token> l = t.betweenLines(1,2);
-            System.out.println(t.tokensToString(l));
+
+            //List<Token> tokens = t.getTokens();
+            //System.out.println(t.tokensToString(tokens));
+
+            // Gets a list of Tokens that come from the HelloWorld.java file between lines 1 and 6.
+            //List<Token> l = t.betweenLines(0,4);
+
+            // Prints out the list of Tokens in lines 1 to 6 in the HelloWorld.java file.
+            //System.out.println(t.tokensToString(l));
+
+
+
+
+            /*========================================================================================================*/
+
+
+            // Testing the String type of constructor.
+
+
+            TokenizerBuilder x = new TokenizerBuilder("package carleton.comps.javaparser.examples;\n" +
+                    "\n" +
+                    "public class HelloWorld {\n" +
+                    "   public static void main(String[] args) { \n" +
+                    "      System.out.println(\"Hello, World\");\n" +
+                    "   }\n" +
+                    "}","String");
+
+            // Prints the tokenized version of HelloWorld.java.
+            //System.out.println(t.getString());
+
+            //List<Token> tokens = t.getTokens();
+            //System.out.println(t.tokensToString(tokens));
+
+            // Gets a list of Tokens that come from the HelloWorld.java file between lines 1 and 6.
+            //List<Token> l = t.betweenLines(3,3);
+
+            // Prints out the list of Tokens in lines 1 to 6 in the HelloWorld.java file.
+            //System.out.println(t.tokensToString(l));
         } catch (IOException e) {
             e.printStackTrace();
         }
