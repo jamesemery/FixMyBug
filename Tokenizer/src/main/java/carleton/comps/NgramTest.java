c4638455ff93a563f4d7384bb1400c1a2b3f9e0a
jamie <emeryj@carleton.edu>
2016-10-30 11:48:42 -0500

    merge conflict

--- Tokenizer/src/main/java/carleton/comps/NgramTest.java
+++ Tokenizer/src/main/java/carleton/comps/NgramTest.java
@@ -18,15 +18,24 @@ public class NgramTest {
             List<Token> c2 = new TokenizerBuilder
                     ("./Tokenizer/src/main/java/carleton/comps/javaparser" +
                             "/examples/HelloWorld.java","File").betweenLines
+<<<<<<< HEAD
                     (0,7);
+=======
+                    (1,5);
+>>>>>>> 11846fc... some readme stuff
             System.out.println(c1.size() + " " + c1);
             System.out.println(c2.size() + " " + c2);
 
-            System.out.println(NgramScorer.scoreSimilarity("public static " +
-                    "void main(String[] args) {", "public static void main" +
-                    "(String[] args) { try {"));
+            System.out.println("Ngram Similarity of c1 and c2 is: " +
+                    NgramScorer.scoreSimilarity(c1,c2,5));
 
-            System.out.println(LevScorer.scoreSimilarity(c1,c2));
+            System.out.println("Ngram Similarity of the strings is: " +
+                    NgramScorer.scoreSimilarity("public static " +
+                            "void main(String[] args) {", "public static void main" +
+                            "(String[] args) { try {"));
+
+            System.out.println("c1 and c2 lev similarity is: " + LevScorer
+                    .scoreSimilarity(c1,c2));
 
         } catch (IOException e) {
             e.printStackTrace();
