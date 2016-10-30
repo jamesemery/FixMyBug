25758a488e64b209b2048ef7a3cc557d4f4bd6cd
Zack <considinez@carleton.edu>
2016-10-29 20:18:14 -0500

    can't work yet with many errors from master branch

--- gs-rest-server/src/main/java/server/DatabaseServer.java
+++ gs-rest-server/src/main/java/server/DatabaseServer.java
@@ -10,10 +10,10 @@ import org.sqlite.SQLiteJDBCLoader;
 import java.sql.*;
 
 public class DatabaseServer {
-    
+
     SQLiteDataSource dataSource;
     String tableName;
-    
+
     public DatabaseServer(String fileName) {
         String url = "jdbc:sqlite:" + fileName;
         System.out.println("Making new database, url = " + url);
@@ -28,7 +28,7 @@ public class DatabaseServer {
         catch (Exception e) {
             System.out.println("Exception caught during database setup: \n");
             System.out.println(e.getMessage());
-        }      
+        }
     }
 
     public static void createNewDatabase(String fileName) {
@@ -47,13 +47,18 @@ public class DatabaseServer {
             System.out.println(e.getMessage());
         }
     }
-    
+
     public final String SelectFrom(String column, String inputStr) {
-        
-        try { 
+        return SelectFrom(column, inputStr, tableName);
+    }
+
+
+    public final String SelectFrom(String column, String inputStr, String table) {
+
+        try {
             System.out.println("Input String: " + inputStr + ".");
             ResultSet rs = dataSource.getConnection().createStatement()
-                    .executeQuery("select " + column + " from \"" + tableName + "\" where buggy_code = \"" + inputStr + "\";");
+                    .executeQuery("select " + column + " from \"" + table + "\" where buggy_code = \"" + inputStr + "\";");
 
             String result = rs.getString(column);
             if(result == null) {
@@ -65,7 +70,7 @@ public class DatabaseServer {
         catch (Exception ex) { //SQLException ex) {
             System.out.println(ex.getMessage());
             return "";
-        }    
+        }
     }
     
     public final DatabaseEntry SelectAll(String inputStr) {
@@ -117,7 +122,7 @@ public class DatabaseServer {
         }
 
     }
-        
+
     public final void RemoveByID(int id) {
         try {
             int rs = dataSource.getConnection().createStatement()
@@ -130,7 +135,7 @@ public class DatabaseServer {
         }
 
     }
-        
+
     public final void RemoveByBug(String bug) {
         try {
             int rs = dataSource.getConnection().createStatement()
@@ -143,19 +148,51 @@ public class DatabaseServer {
         }
 
     }
-    
-    /*public static void main(String[] args) {
-        DatabaseServer db = new DatabaseServer("/FixMyBug/TEST_DATABASE");
-        String fix1 = db.SelectFrom("fixed_code","somebuggg");
-        System.out.println("somefffix - " + fix1);
-        String fix2 = db.SelectAll("somebug2");
-        System.out.println(fix2);
-        db.RemoveByID(4);
-        db.RemoveByID(5);
-        db.RemoveByID(6);
+
+    public void createIdex(int ngramsize, String table) {
+        // Delete the existing index
+        //dataSource.getConnection().createStatement().executeQuery("DROP TABLE table" + "_" + ngramsize + "gramindex;");
+
+        // Create new table
+        dataSource.getConnection().createStatement().executeQuery("CREATE TABLE "+ table + "_"
+        + ngramsize + "gramindex (id INTEGER, hash INTEGER);");
+
+        ResultSet rows = dataSource.getConnection().createStatement().executeQuery("SELECT * from "
+        + table);
+
+        while (rows.next()) {
+            int id = rows.getInt("id");
+            String errCode = rows.getString("error_tokens");
+            String[] tokens = errcode.split(",");
+
+            // populate the the array with each ngram
+            for (int i = ngramsize; i <= tokens.length; i++) {
+                StringBuilder b = new StringBuilder();
+                for (int j = i - ngramsize; j < i; j++) {
+                    b.append(tokens[j] + ",");
+                }
+                int hash = b.toString().hashCode();
+                dataSource.getConnection().createStatement().executeQuery("INSERt INTO"+ table + "_"
+                + ngramsize + "VALUES ("+id+","+hash+");");
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        createIdex(args[0], args[1]);
+        //DatabaseServer db = new DatabaseServer("/FixMyBug/TEST_DATABASE");
+        //String fix1 = db.SelectFrom("fixed_code","somebuggg");
+        //System.out.println("somefffix - " + fix1);
+        //String fix2 = db.SelectAll("somebug2");
+        //System.out.println(fix2);
+        //db.RemoveByID(4);
+        //db.RemoveByID(5);
+        //db.RemoveByID(6);
         //addToTable(args[0], 6, 5, "somebuggg", "somefffix", 2);
         //addToTable(args[0], 4, 2, "somebug2", "somefix2", 2);
         //addToTable(args[0], 5, 4, "somebug3", "somefix3", 2);
-    }*/
+    }
 
 }
