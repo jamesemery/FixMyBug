ee3c6e45dade1ec59fa8a7b8d73df6227643f1a2
leald <leald@carleton.edu>
2016-11-02 17:41:10 -0500

    Adds echo method and refactors code to be easier to use.

--- gs-rest-server/src/main/java/server/FixMyBugController.java
+++ gs-rest-server/src/main/java/server/FixMyBugController.java
@@ -18,22 +18,50 @@ import java.io.*;
 public class FixMyBugController {
 
     @RequestMapping("/fix")
-    public DatabaseEntry clientCode(@RequestBody String input) {
+    public DatabaseEntry fixMyBug(@RequestBody String input) {
+        //Setup the JSON object mapper and our DBConnection
     	ObjectMapper mapper = new ObjectMapper();
         DatabaseServer DBConnection = new DatabaseServer("/FixMyBugDB/TEST_DATABASE");
+
     	try {
     		//Convert JSON string to object
-            ServerRequest server_request = mapper.readValue(input, ServerRequest.class);
+            ServerRequest serverRequest = mapper.readValue(input, ServerRequest.class);
 
-	        System.out.println(server_request.getBuggyCode());
-	        System.out.println(server_request.getErrorMessage());
+	        System.out.println(serverRequest.getBuggyCode());
+	        System.out.println(serverRequest.getErrorMessage());
 	        
 	        // Create a DatabaseEntry and return it.
-	        DatabaseEntry database_entry = DBConnection.SelectAll(server_request.getBuggyCode());
+	        DatabaseEntry database_entry = DBConnection.SelectAll(serverRequest.getBuggyCode());
 	        return database_entry;
 
 
     	} catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new DatabaseEntry(-1, -2, "crap", "squid", -5);
+    }
+
+    @RequestMapping("/test")
+    public DatabaseEntry echo(@RequestBody String input) {
+        //Setup the JSON object mapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            //Convert JSON string to object
+            ServerRequest serverRequest = mapper.readValue(input, ServerRequest.class);
+
+            System.out.println(serverRequest.getBuggyCode());
+            System.out.println(serverRequest.getErrorMessage());
+
+            return new DatabaseEntry(-1, -2, serverRequest.getBuggyCode(), serverRequest.getErrorMessage(), -5);
+
+
+        } catch (JsonGenerationException e) {
             e.printStackTrace();
         } catch (JsonMappingException e) {
             e.printStackTrace();
