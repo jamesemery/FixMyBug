6ef49a6a3ffb0f926605ea5ebbb52e99467f1641
jamie <emeryj@carleton.edu>
2017-01-26 12:41:09 -0600

    attempting to make the diff-match-patch library work

--- DB-Filler/src/main/java/Filler/Main.java
+++ DB-Filler/src/main/java/Filler/Main.java
@@ -4,6 +4,7 @@ package Filler;
 
 import Filler.Tokenizer.DBAscii;
 import Filler.Tokenizer.TokenizerBuilder;
+import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,30 +25,36 @@ public class Main {
         try {
 
             // Testing the file type of the constructor.
-            List<Integer> test1 = java.util.Arrays.asList(0, 100, 30, 25, 26, 27);
-            String test1a = DBAscii.toAsciiFormat(test1);
-            System.out.println("test1a: " + test1a + "   test1a.size() = " + test1a.length());
-            List<Integer> test1b = DBAscii.toIntegerListFromAscii(test1a);
-            System.out.println("test1b: " + test1b);
-            if (test1b.equals(test1)) {
-                System.out.println("Failure");
-            }
-
-            System.out.println("Test2: " + DBAscii.toIntegerListFromAscii("qbds"));
-            System.out.println("Test3: " + DBAscii.toIntegerListFromAscii(""));
+//            List<Integer> test1 = java.util.Arrays.asList(0, 100, 30, 25, 26, 27);
+//            String test1a = DBAscii.toAsciiFormat(test1);
+//            System.out.println("test1a: " + test1a + "   test1a.size() = " + test1a.length());
+//            List<Integer> test1b = DBAscii.toIntegerListFromAscii(test1a);
+//            System.out.println("test1b: " + test1b);
+//            if (test1b.equals(test1)) {
+//                System.out.println("Failure");
+//            }
+//
+//            System.out.println("Test2: " + DBAscii.toIntegerListFromAscii("qbds"));
+//            System.out.println("Test3: " + DBAscii.toIntegerListFromAscii(""));
 
 
             String file1 = new String(Files.readAllBytes(Paths.get
                     ("./src/main/java/Filler/Tokenizer/javaparser/examples" +
                             "/HelloWorld.java")));
+            String file2 = new String(Files.readAllBytes(Paths.get
+                    ("./src/main/java/Filler/Tokenizer/javaparser/examples" +
+                            "/HelloWorld2.java")));
 
-            DBFillerInterface i = new DBFillerInterface("uploadTestDB");
-            System.out.println(i.createDatabaseEntry(file1,file1,3,5,3,5).toStringVerbose());
+//            DBFillerInterface i = new DBFillerInterface("uploadTestDB");
+//            System.out.println(i.createDatabaseEntry(file1,file1,3,5,3,5).toStringVerbose());
             System.out.println("=========================");
 
-            i.uploadToDatabase(file1, file1, 4);
+            DiffMatchPatch differ = new DiffMatchPatch();
+            System.out.println(differ.diffMain(file1,file2));
 
-            System.out.println("DATABASE RETURN:\n\n"+i.SelectAll(1).toStringVerbose());
+//            i.uploadToDatabase(file1, file1, 4);
+
+//            System.out.println("DATABASE RETURN:\n\n"+i.SelectAll(1).toStringVerbose());
 
             // Creates a new instance of TokenizerBuilder.
             //TokenizerBuilder t = new TokenizerBuilder("./src/main/java/carleton/comps/javaparser/examples/HelloWorld.java","File");
